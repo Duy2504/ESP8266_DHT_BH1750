4db4246ce60343b4c6dda5f19bde112c58e35c15
Duy2504 <duyskt2504@gmail.com>
2022-10-30 22:35:56 +0700

    Initial commit

---
+++ Devices/src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <BH1750.h>
+#include <Wire.h>
+#define DHTPIN 0
+#define DHTTYPE DHT11
+#define LED 2
+
+BH1750 lightMeter(0x23);
+
+DHT dht(DHTPIN, DHTTYPE);
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+  Wire.begin();
+
+
+  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE))
+  {
+    Serial.println(F("BH1750 Advanced begin"));
+  }
+  else
+  {
+    Serial.println(F("Error initialising BH1750"));
+  }
+}
+
+void loop()
+{
+  float h = dht.readHumidity();
+  Serial.println(h);
+  float t = dht.readTemperature();
+  Serial.println(t);
+  delay(1000);
+  if (lightMeter.measurementReady())
+  {
+    float lux = lightMeter.readLightLevel();
+    Serial.print("Light: ");
+    Serial.print(lux);
+    Serial.println(" lx");
+  }
+
+  delay(1000);
+}
